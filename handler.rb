4e0ed929bbff61c7e7bc2b04225fd2d3e74a0f59
Paul Dornfeld <pdornfel@gmail.com>
2019-06-18 13:45:31 -0400

    aws skd

--- handler.rb
+++ handler.rb
@@ -1,10 +1,44 @@
 require 'json'
 require 'faker'
+require 'aws-sdk'
+
+Aws.config.update({
+   credentials: Aws::Credentials.new(
+     ENV['access_key_id'],
+     ENV['secret_access_key']
+   )
+})
 
 def hello(event:, context:)
+
+  f_name = Faker::Name.first_name
+  l_name = Faker::Name.last_name
+
+  dynamodb = Aws::DynamoDB::Client.new(region: 'us-east-1')
+
+  item = {
+    firstName: f_name,
+    lastName: l_name,
+    derp: {
+    }
+  }
+
+  params = {
+      table_name: 'myService',
+      item: item
+  }
+
+  begin
+    dynamodb.put_item(params)
+    puts 'Added to table'
+  rescue  Aws::DynamoDB::Errors::ServiceError => error
+    puts 'Unable to add to table'
+    puts error.message
+  end
+
   {
     statusCode: 200,
     body: JSON.generate('Go Serverless v1.0! Your function executed successfully!'),
-    name: Faker::Name.name
+
   }
 end
